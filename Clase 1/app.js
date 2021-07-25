9927a0722d319d125da3588b5a499a850ba74981
Federico Ourens <ourensfederico@gmail.com>
2021-07-25 19:22:30 -0300

    commit

---
+++ Clase 1/app.js
@@ -0,0 +1,26 @@
+console.log("hola mundo")
+
+/* 
+
+Var llamo a una variable y le doy un nombre */ 
+ 
+let numero1 = Number(prompt("Ingresa numero 1"))
+let numero2 = Number(prompt("Ingresa numero 2"))
+
+//toString Number ("Lo que queremos pasar un numero")
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multi = numero1 * numero2
+let divi = numero1 / numero2 
+ 
+console.log(suma);
+console.log(resta);
+console.log(divi);
+console.log(multi);
+
+// console.log y alert para mostrar 
+
+ //Clase 2: Condicionales 
+
+ 
+
