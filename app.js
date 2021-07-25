26bde366958cfdeb0a4cf66d5ea0ad48afcced4a
Federico Ourens <federicoourens@Federicos-MacBook-Pro.local>
2021-07-25 18:54:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("hola mundo")
+
+/* 
+
+Var llamo a una variable y le doy un nombre */ 
+ 
+let numero1 = Number(prompt("Ingresa numero 1"))
+let numero2 = Number(prompt("Ingresa numero 2"))
+
+//toString Number ("Lo que queremos pasar un numero")
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multi = numero1 * numero2
+let divi = numero1 / numero2 
+ 
+console.log(suma);
+console.log(resta);
+console.log(divi);
+console.log(multi);
+
+
+
+ 
+
